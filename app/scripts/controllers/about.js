e71c1ff23ff87d57bbffb56fccce52eab9932569
Artyom Konakov <artkonakov@gmail.com>
2016-07-28 12:32:31 +0300

    with new function

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -10,49 +10,66 @@
 angular.module('kolobashkinApp')
   .controller("AboutCtrl", ['$scope', 'getItems', '$routeParams', function($scope, getItems, $routeParams) {
 
+    $scope.aboutItem = $routeParams.item;
+    $scope.currentMaterial = $routeParams.material;
+
+
+
+
+
     //Делаем запрос к серверу через фабрику
     getItems.getUrl('/items.json').success(function(response) {
+
       $scope.items = response;
 
+      $scope.getArray = function(array, value) {
+
+        for (var i = 0; i < array.length; i++) {
+          console.log(array.length);
+          console.log(value);
+          console.log(array[10].id);
+          if (array[i].id == value) {
+            $scope.currentArray = array[i];
+            console.log($scope.currentArray);
+            return $scope.currentArray;
+          }
+          return console.log('Ошибка!');
+        };
+
+      };
+
+      $scope.getArray($scope.items, $scope.aboutItem);
+
+      $scope.getMaterial = function() {
+
+        for (var i = 0; i < $scope.currentArray.materials.length; i++) {
+          if ($scope.currentArray.materials[i].name == $scope.currentMaterial) {
+
+            $scope.currentArrayMaterial = $scope.currentArray.materials[i];
+            console.log($scope.currentArrayMaterial);
+            return $scope.currentArrayMaterial;
+          }
+          return console.log('Ошибка!');
+        };
+
+      };
+
+      $scope.getMaterial();
+
     });
+
 
 
     getItems.getUrl('/globals.json').success(function(response) {
       $scope.globals = response;
-
     });
 
-    $scope.aboutItem = $routeParams.item;
-
-    $scope.currentMaterial = $routeParams.material;
 
 
-    //Проверяем параметр url чтобы установить текущее положение материала в массиве
-    switch ($scope.currentMaterial) {
-      case 'oak':
-        $scope.index = 0;
-        break;
-      case 'beech':
-        $scope.index = 1;
-        break;
-      case 'birch':
-        $scope.index = 2;
-        break;
-      case 'pine':
-        $scope.index = 3;
-        break;
-    };
 
 
-    function find(array, value) {
 
-      for (var i = 0; i < array.length; i++) {
-        if (array[i] == value) return i;
-      }
 
-      return -1;
-    };
-    
 
 
   }]);
