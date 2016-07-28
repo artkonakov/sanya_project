84ecc0120105a8fa6812647d639fe223987fd1d1
Dizzyspb <derevoluza@mail.ru>
2016-07-28 18:42:35 +0300

    10 items to email

--- app/post.php
+++ app/post.php
@@ -22,7 +22,18 @@ $email=$_POST['email'];
 
 $tel=$_POST['tel'];
 
-$msg=$_POST['message'];
+$msg0=$_POST['message_0'];
+$msg1=$_POST['message_1'];
+$msg2=$_POST['message_2'];
+$msg3=$_POST['message_3'];
+$msg4=$_POST['message_4'];
+$msg5=$_POST['message_5'];
+$msg6=$_POST['message_6'];
+$msg7=$_POST['message_7'];
+$msg8=$_POST['message_8'];
+$msg9=$_POST['message_9'];
+
+$msg=$msg0.$msg1.$msg2.$msg3.$msg4.$msg5.$msg6.$msg7.$msg8.$msg9;
 
 $headers  = "Content-type: text/html; charset=utf-8 \r\n";
 $headers .= "From: Письмо с Колобашкин.ru <from_site@kolobashkin.ru>\r\n";
@@ -82,15 +93,15 @@ fclose($f);
 
 // Выводим сообщение пользователю
 
-#print "<script language='Javascript'><!--
-#function reload() {location = \"$backurl\"}; setTimeout('reload()', 3000);
-//--></script>
+print "<script language='Javascript'><!--
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 12000);
+--></script>
 
 $msg
 
 <p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";
 exit;
 
- }
+}
 
 ?>
